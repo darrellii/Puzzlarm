d59269ffe35d33aa33d3067086aa8c12e6004a8c
deecio <dc0147@gmail.com>
2015-03-29 22:54:02 -0500

    Finished the first build of the puzzle dialog, along with functionality of wrong and right answers. Everything as far as the question goes is hard-coded in though.

--- app/src/main/java/com/project2/anything2/se329/puzzlarm/AlarmReceiverActivity.java
+++ app/src/main/java/com/project2/anything2/se329/puzzlarm/AlarmReceiverActivity.java
@@ -1,6 +1,7 @@
 package com.project2.anything2.se329.puzzlarm;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,11 +15,47 @@ public class AlarmReceiverActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiver);
 
+        final AlertDialog wrongAnswerDialog = new AlertDialog.Builder(this).create();
+        wrongAnswerDialog.setTitle("Wrong Answer");
+        wrongAnswerDialog.setButton("OK", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                wrongAnswerDialog.hide();
+            }
+        });
 
-        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle("WAKE UP");
-        alertDialog.setMessage("What's the capital of New York?");
+        final AlertDialog questionDialog = new AlertDialog.Builder(this).create();
+        questionDialog.setTitle("WAKE UP");
+        questionDialog.setMessage("What's the capital of New York?");
 
+        questionDialog.setButton("Brooklyn", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                wrongAnswerDialog.show();
+            }
+        });
+
+        questionDialog.setButton("Buffalo", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                wrongAnswerDialog.show();
+            }
+        });
+
+        questionDialog.setButton("Syracuse", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+                wrongAnswerDialog.show();
+            }
+        });
+
+        // If the right answer is selected, it just removes the dialog alert,
+        // allowing the user to access the alarm page.
+
+        questionDialog.setButton("Albany", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                questionDialog.hide();
+            }
+        });
+
+        questionDialog.show();
 
     }
 
